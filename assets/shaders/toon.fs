ab2497f509c638ad51745a7bf19a66127ea023b6
IkoMerkel <iko.merkel@mymail.champlain.edu>
2026-02-09 17:56:20 -0500

    Fixed the cell shade to not repeat

--- assets/shaders/toon.fs
+++ assets/shaders/toon.fs
@@ -14,6 +14,13 @@ struct Palette{
   vec3 color2;
 };
 
+struct Material{
+  float ambient;
+  float diffuse;
+  float specular;
+  float shiny;
+};
+
 // varyings
 in vec3 vs_position;
 in vec3 vs_normal;
@@ -24,6 +31,7 @@ uniform Light light;
 uniform sampler2D texture0;
 uniform sampler2D texture1;
 uniform Palette pal;
+uniform Material material;
 
 vec3 toon(vec3 normal, vec3 frag_position, vec3 light_position) {
   vec3 view_dir = normalize(camera - frag_position);
@@ -31,11 +39,11 @@ vec3 toon(vec3 normal, vec3 frag_position, vec3 light_position) {
   vec3 reflect_dir = reflect(light_dir, vs_normal);
   vec3 half_dir = normalize(light_dir + view_dir); 
 
-  float NdotL = dot(normal, light_dir) + 1.0f * 0.5f;
-  float NdotH = max(dot(normal, half_dir), 0.0f);
+  float NdotL = (dot(normal, light_dir) + 1.0f) * 0.5f;
+  float NdotH = pow(max(dot(normal, half_dir), 0.0f), material.shiny);
   float PdotL = dot(vs_position, light_position.xyz);
 
-  vec3 gradient = texture(texture0,vec2(NdotL,NdotH)).rgb;  
+  vec3 gradient = texture(texture0,vec2(NdotL,NdotL)).rgb;  
   vec3 light_color = mix(pal.color1,pal.color2,gradient);
 
   float diffuse = NdotL;
